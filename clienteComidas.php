f46983ad92cdfb7bb5dd53a26b39992fcb7be008
William8400 <wdonato02@gmail.com>
2025-10-23 17:29:13 -0300

    fazendo a url do cliente

--- clienteComidas.php
+++ clienteComidas.php
@@ -1,5 +1,16 @@
 <?php
+// saber na documentação qual a URL
 
+$url = "http://localhost/reforco_API/comidas.API.php?comida=cuzcuz";
+
+// Requisitar uma resposta da URL
+$resposta = file_get_contents($url);
+
+$feijoada = json_decode($resposta, true);
+
+echo "<h1> FEIJOADA </h1>";
+echo "<h3> Ingredientes: ".$feijoada['ingredientes']."</h3>";
+echo "<h3> Modo de preparo:".$feijoada['modo de preparo']."</h3>";
 
 
 
