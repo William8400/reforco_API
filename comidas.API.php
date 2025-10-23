ac9547bd76b57f02e70cffed746a6f7291946599
William8400 <wdonato02@gmail.com>
2025-10-23 17:10:51 -0300

    Criando API

---
+++ comidas.API.php
@@ -0,0 +1,60 @@
+<?php
+
+// Cabeçalho da API - Metadados 
+
+
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+
+// SISTEMA DA API 
+
+$metodo = $_SERVER["REQUEST_METHOD"]; // Guardar o metodo que foi requisitado pelo cliente
+
+switch($metodo){
+    case "GET":
+         metodoGET();
+        break;
+
+    case "POST":
+
+        break;
+    
+    default:
+        echo "Método inválido";
+}
+
+function metodoGET(){
+
+    // Manipulação do arquivo JSON
+
+    // Variável para guardar os dados do arquivo JSON no formato Array associativo
+    $comidas = json_decode(file_get_contents("comidas.json"), true );
+
+    //Variável para guardar o parâmetro (query string) passado no EndPoint da url
+    $escolha_cliente = $_GET['comida'];
+
+    if($escolha_cliente == "feijoada"){
+
+        // Variável para guardar o valor especifico do Array inteiro que temos
+        $valor_cliente = $comidas['comidas']['feijoada'];
+
+        // Imprimir em formato JSON o valor da variável para ser uma resposta pro cliente
+        echo json_encode($valor_cliente);
+
+    }else if($escolha_cliente == "cuzcuz"){
+
+        $valor_cliente = $comidas['comidas']['cuzcuz'];
+        echo json_encode($valor_cliente);
+
+    }else{
+
+        // Caso não atenda a nenhuma opção, ele retorna todas as comidas.
+        echo json_encode($comidas);
+
+    } 
+
+}
+
+
+
+?>
