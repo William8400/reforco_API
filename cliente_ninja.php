a9fbd0c26237fa3f425da70fbd9a4af8cdf0f63b
William8400 <wdonato02@gmail.com>
2025-10-30 14:44:57 -0300

    Consumindo api

--- cliente_ninja.php
+++ cliente_ninja.php
@@ -2,3 +2,22 @@
 
 $url = "http://localhost/reforco_API/api_ninja.php";
 
+$chave_ninja = [
+    'codigo' => 'ADREREG134'
+];
+
+$estrutura_http = [
+    'http' => [
+        'method' => "POST",
+        'header' => "Content-Type: application/json\r\n",
+        'content' => json_encode($chave_ninja)
+    ]
+];
+
+$contexto = stream_context_create($estrutura_http);
+
+$resposta = file_get_contents($url, false, $contexto);
+
+echo $resposta;
+
+?>
