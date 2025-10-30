8636b166da1a90beee793c1afc143d0879c4176a
William8400 <wdonato02@gmail.com>
2025-10-30 17:29:52 -0300

    .

--- Secreta/api_Secreta.php
+++ Secreta/api_Secreta.php
@@ -7,14 +7,23 @@ $metodo = $_SERVER['REQUEST_METHOD'];
 
 switch($metodo){
     case "GET":
-
+        echo json_encode("Para você acessar o código secreto você tem que digitar ?123 no endpoint da url");
         break;
 
     case "POST":
-
+        valor_Secreto();
         break;
 
     default:
 
         break;
+}
+
+function valor_Secreto(){
+
+    $valor = json_decode(file_get_contents("php://input"), true);
+
+    if ($valor['codigo'] === 242424) {
+        echo json_encode("Este é o código secreto");
+    }
 }
--- Secreta/cliente_Secreta.php
+++ Secreta/cliente_Secreta.php
@@ -1,2 +1,21 @@
 <?php 
 
+$url = "http://localhost/reforco_API/Secreta/api_Secreta.php?codigo=242424";
+
+$codigo_Secreto = [
+    ''
+];
+
+$estrutura_HTTP = [
+    'http' => [
+        'method' => "POST",
+        'header' => "Content-Type: application/json\r\n",
+        'content' => json_encode($codigo_Secreto)
+    ]
+];
+
+$contexto = stream_context_create($estrutura_HTTP);
+
+$resposta = file_get_contents($url, false, $contexto);
+
+echo $resposta;
