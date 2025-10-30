05aac75ad7a5f85f55a00cfd6eaad725af2d7ea6
William8400 <wdonato02@gmail.com>
2025-10-30 14:16:37 -0300

    começando api

--- api_ninja.php
+++ api_ninja.php
@@ -2,6 +2,33 @@
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 
+$metodo = $_SERVER['REQUEST_METHOD'];
+
+switch($metodo){
+    case "GET":
+        echo json_encode("Metodo GET consultado com sucesso!");
+        break;
+
+    case "POST":
+        verificar_codigo_secreto();
+        break;
+
+    case "PUT";
+
+        break;
+
+    case "DELETE":
+
+        break;
+
+    default:
+
+        break;
+    
+}
 
 
+function verificar_codigo_secreto(){
+    
+}
 ?>
