c786cbc716418e357cef2604e6e08cda5f881898
William8400 <wdonato02@gmail.com>
2025-11-10 16:43:32 -0300

    Update cliente.php

--- cardapio-De-Restaurante-XML/cliente.php
+++ cardapio-De-Restaurante-XML/cliente.php
@@ -7,6 +7,8 @@ echo "<h1> Cardápio do Restaurante </h1>";
 foreach ($cardapio as $comida){
     echo "<hr>";
 
+    echo " <h2> Café da Manhã: </h2>";
+
     echo "<p><b>Nome:</b> {$comida->name}</p>";
     echo "<p><b>Preço:</b> {$comida->price}</p>";
     echo "<p><b>Descrição:</b> {$comida->description}</p>";
