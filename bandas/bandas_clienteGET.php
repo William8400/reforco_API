06a0492348868c26dc69ba6dbed1a70807db026d
William8400 <wdonato02@gmail.com>
2025-11-10 13:46:40 -0300

    Update bandas_clienteGET.php

--- bandas/bandas_clienteGET.php
+++ bandas/bandas_clienteGET.php
@@ -1,15 +1,15 @@
 <?php
 
-$url = "http://localhost/reforco_API/bandas/bandas_API.php?banda=Mamonas_Assassinas";
+$url = "http://localhost/reforco_API/bandas/bandas_API.php?banda=Queen";
 
 $resposta = file_get_contents($url);
 
 $banda = json_decode($resposta, true);
 
-echo "<h1> Mamonas Assassinas </h1>";
+echo "<h1> Queen </h1>";
 echo "<p><b> Origem: </b> ".$banda['origem']."</p>";
 echo "<p><b> Genero:</b> ".$banda['genero'][0]."</p>";
 echo "<p><b> Ano Formação:</b> ".$banda['ano_formacao']."</p>";
-echo "<p><b> Ano Termino:</b> ".$banda['ano_termino']."</p>";
+// echo "<p><b> Ano Termino:</b> ".$banda['ano_termino']."</p>";
 echo "<p><b> Descrição:</b> ".$banda['descricao']."</p>";
 
