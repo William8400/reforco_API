b66ea40d18649a0943f4bab59bd096db6ed3102b
William8400 <wdonato02@gmail.com>
2025-11-10 13:43:01 -0300

    Update bandas_clienteGET.php

--- bandas/bandas_clienteGET.php
+++ bandas/bandas_clienteGET.php
@@ -4,12 +4,12 @@ $url = "http://localhost/reforco_API/bandas/bandas_API.php?banda=Mamonas_Assassi
 
 $resposta = file_get_contents($url);
 
-$Mamonas_Assassinas = json_decode($resposta, true);
+$banda = json_decode($resposta, true);
 
 echo "<h1> Mamonas Assassinas </h1>";
-echo "<p><b> Origem: </b> ".$Mamonas_Assassinas['origem']."</p>";
-echo "<p><b> Genero:</b> ".$Mamonas_Assassinas['genero'][0]."</p>";
-echo "<p><b> Ano Formação:</b> ".$Mamonas_Assassinas['ano_formacao']."</p>";
-echo "<p><b> Ano Termino:</b> ".$Mamonas_Assassinas['ano_termino']."</p>";
-echo "<p><b> Descrição:</b> ".$Mamonas_Assassinas['descricao']."</p>";
+echo "<p><b> Origem: </b> ".$banda['origem']."</p>";
+echo "<p><b> Genero:</b> ".$banda['genero'][0]."</p>";
+echo "<p><b> Ano Formação:</b> ".$banda['ano_formacao']."</p>";
+echo "<p><b> Ano Termino:</b> ".$banda['ano_termino']."</p>";
+echo "<p><b> Descrição:</b> ".$banda['descricao']."</p>";
 
