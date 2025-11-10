ea419df1efa5c5f94bcc7c1d0d75f22ca330db29
William8400 <wdonato02@gmail.com>
2025-11-10 15:48:44 -0300

    -

---
+++ exercicio01/cliente.php
@@ -0,0 +1,15 @@
+<?php
+
+$cardapio = simplexml_load_file('info.xml');
+
+echo "<h1> Cardápio do Restaurante </h1>";
+
+foreach ($cardapio as $comida){
+    echo "<hr>";
+
+    echo "<p><b>Nome:</b> {$comida->name}</p>";
+    echo "<p><b>Preço:</b> {$comida->price}</p>";
+    echo "<p><b>Descrição:</b> {$comida->description}</p>";
+    echo "<p><b>Calorias:</b> {$comida->calories}</p>";
+
+}
